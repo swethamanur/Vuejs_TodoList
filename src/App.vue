80235fa1346ae3d02a60b5fcaa50fa92d9e3829f
swetha manur <swetham@desertlabstudio.com>
2019-08-09 15:05:52 -0700

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,16 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
-
 export default {
   name: "app",
-  components: { Todos, Header, AddTodo },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(res => {
-          this.todos = this.todos.filter(t => {
-            return t.id !== id;
-          });
-        })
-        .catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios
-        .post("https://jsonplaceholder.typicode.com/todos", {
-          title,
-          completed
-        })
-        .then(res => {
-          console.log(res.data);
-          this.todos = [...this.todos, res.data];
-        })
-        .catch(err => console.log(err));
-    }
-  },
-
-  //similar to ComponentDiDMount in React js
-  created: function() {
-    // `this` points to the vm instance
-    console.log("a is: " + this.a);
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then(res => {
-        console.log("axios");
-        this.todos = res.data;
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  components: {
+    Header
   }
 };
 </script>
